8646e3e8dfad360591640c9f569cb6f466d0e68b
yigitKomsu <ihsanyigitergin@gmail.com>
2016-08-26 12:11:30 +0300

    sadece boss fight olsun be

---
+++ Assets/Levels/Boss/BossFightSpawner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossFightSpawner : MonoBehaviour {
+	public GameObject Toaster;
+	Terminal Terminal;
+	float time;
+	int mock;
+	// Use this for initialization
+	void Start () {
+		Terminal = GameObject.Find ("boss").GetComponent<Terminal> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		mock = Random.Range (0, 5);
+		if (time >= 3 && mock <= 1) {
+			Instantiate (Toaster, new Vector2 (0, 5), Quaternion.identity);
+			Terminal.changeText (1);
+			time = 0;
+		} else if (time >= 3 && mock > 1) {
+			Terminal.changeText (mock);
+			time = 0;
+		}
+	}
+}
--- Assets/Levels/Boss/Terminal.cs
+++ Assets/Levels/Boss/Terminal.cs
@@ -3,14 +3,17 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class Terminal : MonoBehaviour {
-
+	public GameObject Computer;
 	public Text terminalText;
-	int i;
+	int i, lineCount;
 
 	string[] texts = {
-		"echo \"Hello, World!\"\n>Hello, World!",
-		"set Toast",
-		"Toast kill"
+		"echo \"Hello, World!\"\n>Hello, World! \n>Computer.Protect();",
+		"Toaster.SetActive(true);",
+		"BobsPC.InTheFace();",
+		"echo \"Oh my..\"\n>Oh my..",
+		"BurnInCoffee();",
+		"Clear();"
 	};
 
 	string currText;
@@ -33,14 +36,26 @@ public class Terminal : MonoBehaviour {
 			terminalText.text += currText[i].ToString();	
 			i++;
 		} else {
-			//changeText (1);	
+			//Behey
 		}
+
+		if (lineCount > 15)
+			clearText ();
+
+		if (Computer == null)
+			texts [2] = "NullReferanceException: Object referance not set to an instance of an object!";
 	}
 		
 	public void changeText(int index) {
 		currText = texts [index];
 		i = 0;
 		terminalText.text += "\n>";
-
+		lineCount++;
 	} 
+
+	void clearText(){
+		changeText (5);
+		terminalText.text = ">";
+		lineCount = 0;
+	}
 }
