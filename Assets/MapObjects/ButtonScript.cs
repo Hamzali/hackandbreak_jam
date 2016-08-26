927dcd09beb1cf1f4ee2826fb21e43ecca57ed21
yigitKomsu <ihsanyigitergin@gmail.com>
2016-08-26 15:57:23 +0300

    bitti valla

---
+++ Assets/MapObjects/ButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+	GameObject Wall;
+	public GameObject pc;
+
+	void Start(){
+		pc = GameObject.Find ("pc_0");
+	}
+
+	public void StartGame(){
+		Wall = GameObject.Find ("TestGround (1)");
+		Wall.SetActive (false);
+		Wall = GameObject.Find ("TestGround (2)");
+		Wall.SetActive (false);
+		pc.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2(-17000, 17000));
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.tag == "Player")
+			EditorSceneManager.LoadScene (1);
+	}
+}
